359ec97029c54435b5d0b8775a57e63ee88d0162
Your Name <you@example.com>
2017-07-20 20:08:20 +0200

    adds entry to window

--- main.py
+++ main.py
@@ -4,26 +4,35 @@ import Tkinter
 import keybinder
 import gtk
 
-keystr = "<Ctrl>A"
+keystr = "<Ctrl>I"
 
 wind = gtk.Window()
+wind.set_decorated(False)
+
+vbox = gtk.VBox()
+
+wind.add(vbox)
+
+entry = gtk.Entry()
+
 butt = gtk.Button("Hide")
 
-wind.add(butt)
-
-butt.show()
+vbox.add(entry)
+vbox.add(butt)
 
 def onclick(event):
     wind.hide()
     
 butt.connect("clicked", onclick)
 
-def callback( key):
+def callback():
     if wind.get_visible():
         wind.hide()
     else:
-        wind.show()
+        wind.show_all()
     
-keybinder.bind(keystr, callback, "Bla")
+keybinder.bind(keystr, callback)
+
+wind.show_all()
 
 gtk.main()
