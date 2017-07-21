37f922bb56e0fc81cca18b557d0d40411ecb85e2
Your Name <you@example.com>
2017-07-21 13:13:18 +0200

    adds ResultBox widget

--- main.py
+++ main.py
@@ -6,6 +6,58 @@ import gtk
 import random
 
 keystr = "<Ctrl>I"
+file_path = "/tmp/python_blub"
+
+class ResultBox(gtk.VBox):
+    def __init__(self):
+        super(self.__class__, self).__init__()
+        self.last_query = ""
+        self.last_slice = []
+
+        self.load_content()
+
+    def load_content(self):
+        self.content = []
+        with open(file_path, "r") as f:
+            for line in f.readlines():
+                if line.strip():
+                    self.content.append(line.strip())
+
+    def add_entry(self, entry):
+        self.content.append(entry)
+
+    def save_content(self):
+        with open(file_path, "w") as f:
+            f.write("\n".join(self.content))
+
+    def update(self, string):
+        self.destroy_children()
+
+        if not string:
+            return
+
+        lst = [ item for item in self.content if item.find(string) >= 0 ]
+
+        for item in lst:
+            index = item.find(string)
+            end = index+len(string)
+
+            label = gtk.Label()
+            label.set_justify(gtk.JUSTIFY_LEFT)
+            label.set_use_markup(True)
+            label.set_markup(item[0:index] + "<b>" + item[index:end] + "</b>" + item[end:])
+
+            lalign = gtk.Alignment(0,0,0,0)
+            lalign.set_padding(0,0,5,5)
+
+            lalign.add(label)
+
+            self.add(lalign)
+
+    def destroy_children(self):
+        for child in self.get_children():
+            child.destroy()
+
 
 wind = gtk.Window()
 wind.set_decorated(False)
@@ -16,7 +68,7 @@ wind.add(vbox)
 
 entry = gtk.Entry()
 
-found_entries = gtk.VBox()
+found_entries = ResultBox()
 
 vbox.add(entry)
 vbox.add(found_entries)
@@ -34,9 +86,7 @@ def callback():
         wind.show_all()
 
 def on_change(widget):
-
-    for child in found_entries.get_children():
-        child.destroy()
+    found_entries.update(widget.get_text())
 
     wind.resize(2,2)
 
