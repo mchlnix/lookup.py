ff04730d7cae0358cfe51471d5362c578764c4e3
Michael Nix <mchl.nix@googlemail.com>
2017-07-22 11:05:26 +0200

    breaks out the content from the ResultBox widget

--- ResultBox.py
+++ ResultBox.py
@@ -1,5 +1,65 @@
 import gtk
 
+class IContentProvider:
+    def __init__(self):
+        pass
+
+    def open(self):
+        raise NotImplementedError
+
+    def add(self, item):
+        raise NotImplementedError
+
+    def reload(self):
+        raise NotImplementedError
+
+    def remove(self, item):
+        raise NotImplementedError
+
+    def close(self):
+        raise NotImplementedError
+
+    def get_all(self, query):
+        raise NotImplementedError
+
+    def highlight(self, item, query):
+        raise NotImplementedError
+
+class FileContentProvider(IContentProvider):
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.content = []
+
+    def open(self):
+        try:
+            with open(self.file_path, "r+") as f:
+                for line in f.readlines():
+                    if line.strip():
+                        self.content.append(line.strip())
+        except IOError as e:
+            print e
+
+    def add(self, item):
+        if not entry in self.content:
+            self.content.append(entry)
+
+    def reload(self):
+        self.open()
+
+    def close(self):
+        with open(self.file_path, "w") as f:
+            f.write("\n".join(self.content))
+
+    def get_all(self, query):
+        return [ item for item in self.content if item.lower().find(query.lower()) >= 0 ]
+
+    def hightlight(self, item, query):
+        index = item.lower().find(query.lower())
+        end = index+len(query)
+
+        return item[0:index] + "<b>" + item[index:end] + "</b>" + item[end:]
+
+
 class ResultBox(gtk.VBox):
     def __init__(self, file_path):
         super(self.__class__, self).__init__()
